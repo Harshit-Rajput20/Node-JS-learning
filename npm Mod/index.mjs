8c7c8752198dd66800b3c9a910f8b75d80d1f3d0
Harshit-Rajput20 <harshitrajput7782gmail.com>
2023-09-03 14:34:02 +0530

    Npm  basics

---
+++ npm Mod/index.mjs
@@ -0,0 +1,8 @@
+// const chalk = require("chalk");
+import chalk from "chalk";
+// const val = require("validator");
+
+import val from "validator";
+console.log(chalk.red("hello world"));
+
+console.log(val.isEmail("h@h.com"));
