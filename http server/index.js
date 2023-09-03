e1320745094e83c39db82acb9cbbac48665ae32b
Harshit-Rajput20 <harshitrajput7782gmail.com>
2023-09-03 21:44:43 +0530

    Node.js routing

--- http server/index.js
+++ http server/index.js
@@ -1,7 +1,16 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  res.end("hello this is my first server");
+  if (req.url == "/") {
+    res.end("hello this is my first server");
+  } else if (req.url == "/about") {
+    res.end("hello this is from about side");
+  } else if (req.url == "/contact") {
+    res.end("hello this is from contact side");
+  } else {
+    res.writeHead(404);
+    res.end("page doest ot exit");
+  }
 });
 
 server.listen(8000, "127.0.0.1", () => {
