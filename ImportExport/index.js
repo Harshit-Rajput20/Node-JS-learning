2d8e3dd532093e92f526e819a1ed0b2fa91219b5
Harshit-Rajput20 <harshitrajput7782gmail.com>
2023-09-03 12:00:44 +0530

    exporting your own module

---
+++ ImportExport/index.js
@@ -0,0 +1,11 @@
+// const opr = require("./opr");
+
+const { add, mult, sub } = require("./opr");
+
+// console.log(opr.add(5, 7));
+// console.log(opr.sub(10, 5));
+
+console.log(add(5, 7));
+console.log(sub(10, 5));
+
+console.log(mult(2, 8));
