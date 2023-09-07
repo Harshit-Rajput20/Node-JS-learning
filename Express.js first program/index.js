3742daf2425d979af367003b243de7b0586e1e06
Harshit-Rajput20 <harshitrajput7782gmail.com>
2023-09-07 22:39:31 +0530

    use of view  engine in express js

--- Express.js first program/index.js
+++ Express.js first program/index.js
@@ -1,13 +1,25 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 
-app.get("/", (req, res) => {
-  res.send("hello this is from server");
+// const staticPath = path.join(__dirname, "/public");
+app.set("view engine", "hbs");
+// app.use(express.static(staticPath));
+
+app.get("", (req, res) => {
+  res.render("index");
 });
 
 app.get("/about", (req, res) => {
-  res.send("this is abut page");
+  res.render("about");
 });
+// app.get("/", (req, res) => {
+//   res.send("hello this is from server");
+// });
+
+// app.get("/about", (req, res) => {
+//   res.send("this is abut page");
+// });
 
 app.listen(8000, () => {
   console.log("listening from the server");
